61d3c0001a059bde1681c48a49aad8ec506fd462
dfoerster79 <46315137+dfoerster79@users.noreply.github.com>
2026-05-30 14:57:18 +0200

    feat: Route /admin/schulen eingetragen

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import RegisterPage from './pages/RegisterPage.jsx'
 import ProfileSetupPage from './pages/ProfileSetupPage.jsx'
 import DashboardPage from './pages/DashboardPage.jsx'
 import AdminPage from './pages/AdminPage.jsx'
+import SchulenPage from './pages/SchulenPage.jsx'
 import SchulenImportPage from './pages/SchulenImportPage.jsx'
 import OrteImportPage from './pages/OrteImportPage.jsx'
 import NotFoundPage from './pages/NotFoundPage.jsx'
@@ -18,6 +19,7 @@ export default function App() {
       <Route path="/profil-einrichten" element={user ? <ProfileSetupPage /> : <Navigate to="/" />} />
       <Route path="/dashboard" element={user ? <DashboardPage /> : <Navigate to="/" />} />
       <Route path="/admin" element={user ? <AdminPage /> : <Navigate to="/" />} />
+      <Route path="/admin/schulen" element={user ? <SchulenPage /> : <Navigate to="/" />} />
       <Route path="/admin/schulen-import" element={user ? <SchulenImportPage /> : <Navigate to="/" />} />
       <Route path="/admin/orte-import" element={user ? <OrteImportPage /> : <Navigate to="/" />} />
       <Route path="*" element={<NotFoundPage />} />
