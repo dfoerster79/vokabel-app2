371f83dfb88b1233c21be65cf22a0788fe94c36a
dfoerster79 <46315137+dfoerster79@users.noreply.github.com>
2026-07-19 11:58:00 +0200

    Add AdminFachuebersichtPage route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import ProfilPage from './pages/ProfilPage.jsx'
 import NotFoundPage from './pages/NotFoundPage.jsx'
 import FotoTestPage from './pages/FotoTestPage.jsx'  // NEU
 import { useAuthStore } from './store/authStore.js'
+import AdminFachuebersichtPage from './pages/AdminFachuebersichtPage.jsx'
 
 export default function App() {
   const user = useAuthStore(s => s.user)
@@ -21,6 +22,7 @@ export default function App() {
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/profil-einrichten" element={user ? <ProfileSetupPage /> : <Navigate to="/" />} />
       <Route path="/dashboard" element={user ? <DashboardPage /> : <Navigate to="/" />} />
+      <Route path="/admin/fachuebersicht" element={<AdminFachuebersichtPage />} />
       <Route path="/profil" element={user ? <ProfilPage /> : <Navigate to="/" />} />
       <Route path="/neuer-test" element={user ? <FotoTestPage /> : <Navigate to="/" />} />  {/* NEU */}
       <Route path="/admin" element={user ? <AdminPage /> : <Navigate to="/" />} />
